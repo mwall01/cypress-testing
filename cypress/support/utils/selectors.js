19ab7bf16863a503c312461627bd7329ebff2744
Michael Wall <48205225+mwall01@users.noreply.github.com>
2023-08-18 12:43:13 +0100

    Added selectors file & acuity tests

---
+++ cypress/support/utils/selectors.js
@@ -0,0 +1,19 @@
+export const selectors = {
+  // Acuity
+  emergingTitle: '[itemprop="name emerging"]',
+  emergingSignUpButton: '#emerging-signup-button',
+  acuityLogo: '.menu-container > .logo > img',
+  trySchedulingButton: '.hero-mvp-cta-box',
+  signUpButton: '#sign-up-nav-button',
+  languageSelector: '.language-selector',
+  pageTitle: '.new-hero-title',
+
+  // Squarespace
+  logo: '.hero__text > h1',
+  productsDropdown: 'button[aria-label="Products dropdown menu"]',
+  resourcesDropdown: '[aria-label="Resources dropdown menu"]',
+  domainSearch: '[placeholder="Start your search here"]',
+  domainResult: '.domain-name-search__category-list--featured > :nth-child(1) > .domain-name-search__list-item-wrapper-button',
+  total: '.total',
+  checkoutButton: '.checkout'
+}
